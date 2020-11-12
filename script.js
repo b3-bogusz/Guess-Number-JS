3c4ca5f5f548947f0b113879a32d29f25c9e19f2
b3-bogusz <b3.bogusz@gmail.com>
2020-11-12 22:52:02 +0100

    adding functionality to 'AGAIN' button

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 'use strict';
 
-// document.querySelector('.message').textContent = '⚡ ️Correct Number!';
-// document.querySelector('.guess').value = 0;
 
-const secretNumber = Math.floor(Math.random() * 5) + 1;
-// document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.floor(Math.random() * 5) + 1;
 
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess);
+
 
     if (!guess) {
         document.querySelector('.message').textContent = '❌ No Number!';
 
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '⚡ ️Correct Number!';
+      document.querySelector('.number').textContent = guess;
         document.querySelector('body').style.backgroundColor = '#35a14e';
         document.querySelector('.number').style.width = '30rem';
 
@@ -41,13 +39,24 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.message').textContent = '🔥 You lost the game!';
         document.querySelector('.score').textContent = 0;
       }
-
     }
 });
 
 
 
+document.querySelector('.again').addEventListener('click', function(){
 
+  score = 20
+  secretNumber = Math.floor(Math.random() * 5) + 1;
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.guess').value = "";
+  document.querySelector('.message').textContent = 'Start guessing..';
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
 
 
 
